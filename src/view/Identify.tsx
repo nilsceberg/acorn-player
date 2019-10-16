b0d6321bbab4abe46a6d87c52ea57fe7d6702986
Nils Ceberg <nils.ceberg@gmail.com>
2019-10-16 18:01:55 +0200

    Make identify screen a bit more pleasant to look at

--- src/view/Identify.tsx
+++ src/view/Identify.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 export const Identify = (props: { children: string; }): JSX.Element => (
-	<div style={{ position: "fixed", left: 0, right: 0, top: 0, bottom: 0, backgroundColor: "#00ff00", textAlign: "center" }}>
+	<div style={{ position: "fixed", left: 0, right: 0, top: 0, bottom: 0, backgroundColor: "#2e3440", color: "white", textAlign: "center" }}>
 		<h1 style={{ fontSize: 88, textTransform: "uppercase" }}>
 			{props.children}
 		</h1>
