33f01102531c61bea4f6634af4bbd0e3fb3d556f
Nils Ceberg <nils.ceberg@gmail.com>
2019-10-17 19:12:22 +0200

    Improve system message positioning

--- src/view/SystemMessage.tsx
+++ src/view/SystemMessage.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export const SystemMessage = (props: { children: string; }): JSX.Element => (
 	<div style={{ position: "fixed", left: 0, right: 0, top: 0, bottom: 0, backgroundColor: "#2e3440", color: "white", textAlign: "center" }}>
-		<h1 style={{ fontSize: 46, textTransform: "uppercase" }}>
+		<h1 style={{ fontSize: 46, textTransform: "uppercase", marginTop: 250 }}>
 			{props.children}
 		</h1>
 	</div>
