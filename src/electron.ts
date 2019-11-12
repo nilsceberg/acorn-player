febedc714da6c1768fff116f764eb3c8848b7240
Nils Ceberg <nils.ceberg@gmail.com>
2019-11-12 18:03:36 +0100

    filter out X-Frame-Options

--- src/electron.ts
+++ src/electron.ts
@@ -1,13 +1,26 @@
 import { app, BrowserWindow } from "electron";
 
 async function createWindow() {
-	const win = new BrowserWindow({
+	let win = new BrowserWindow({
 		width: 800,
 		height: 600,
 		autoHideMenuBar: true,
 		webPreferences: {
 			nodeIntegration: true,
 		}
+	});
+
+	var onHeadersReceived = (d: any, c: any) => {
+		if (d.responseHeaders['X-Frame-Options']) {
+			delete d.responseHeaders['X-Frame-Options'];
+		}
+		c({ cancel: false, responseHeaders: d.responseHeaders });
+	}
+	win.webContents.session.webRequest.onHeadersReceived(onHeadersReceived);
+
+	win.on('closed', function () {
+		win.removeAllListeners();
+		win = null;
 	});
 
 	//await win.loadFile("../static/index.html");
